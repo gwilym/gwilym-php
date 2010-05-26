a42efcf95a77286719c54e3567c6ea47b61131ae
gwilym <github@whoah.net>
2010-05-26 22:43:21 +1000

    no need to calculate isWindows every time but still want it in a class, so, make it just return a definition (the defined _GWILYM_PHP_ISWINDOWS shouldn't be used directly though as it won't be loaded)

--- lib/Gwilym/PHP.php
+++ lib/Gwilym/PHP.php
@@ -1,5 +1,7 @@
 <?php
 
+define('_GWILYM_PHP_ISWINDOWS', substr(PHP_OS, 0, 3) == 'WIN');
+
 /**
 * This class implements various methods for checking on PHP capabilities, the OS we're on, etc.
 */
@@ -9,10 +11,6 @@ class Gwilym_PHP
 
 	public static function isWindows ()
 	{
-		if (self::$_isWindows === null)
-		{
-			self::$_isWindows = substr(PHP_OS, 0, 3) == 'WIN';
-		}
-		return self::$_isWindows;
+		return _GWILYM_PHP_ISWINDOWS;
 	}
 }
