91620d95c29a4d8390047de0772321e960e0c580
gwilym <github@whoah.net>
2010-05-26 22:37:07 +1000

    add factory method for keystore

--- lib/Gwilym/KeyStore.php
+++ lib/Gwilym/KeyStore.php
@@ -2,5 +2,14 @@
 
 class Gwilym_KeyStore extends Gwilym_KeyStore_File
 {
+	protected static $_instance;
 
+	public static function factory ()
+	{
+		if (self::$_instance === null)
+		{
+			self::$_instance = new Gwilym_KeyStore_File();
+		}
+		return self::$_instance;
+	}
 }
