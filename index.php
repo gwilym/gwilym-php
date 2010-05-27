d55f495af2e7c2c2b567297f099eec9515a81782
gwilym <github@whoah.net>
2010-05-27 23:44:39 +1000

    using router flexibility to add fixed routes to index controller while the more complex router is loaded lazily for other requests

--- index.php
+++ index.php
@@ -3,5 +3,16 @@
 require_once(dirname(__FILE__) . '/bootstrap.php');
 
 $request = new Gwilym_Request;
-$request->addRouter(new Gwilym_Router_Standard_Reverse);
+
+// fixed routes directly from / to Index
+$fixed = new Gwilym_Router_Fixed;
+$fixed->addFixedRoute('', 'Controller_Index');
+$fixed->addFixedRoute('/', 'Controller_Index');
+$fixed->addFixedRoute('/index.php', 'Controller_Index');
+$request->addRouter($fixed);
+
+// fallback for dynamic routes using lazy router load
+$request->addRouter('Gwilym_Router_Standard_Reverse');
+
+// go
 $request->handle();
