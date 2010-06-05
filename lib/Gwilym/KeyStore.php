f252453d8c4d63b430c60a763d03ae211e641ae9
gwilym <github@whoah.net>
2010-06-05 18:04:38 +1000

    implement base prefix setting and locking, change factory method to use configured default keystore type

--- lib/Gwilym/KeyStore.php
+++ lib/Gwilym/KeyStore.php
@@ -1,15 +1,39 @@
 <?php
 
-class Gwilym_KeyStore extends Gwilym_KeyStore_File
+abstract class Gwilym_KeyStore
 {
 	protected static $_instance;
 
+	/** @return Gwilym_KeyStore_Interface */
 	public static function factory ()
 	{
 		if (self::$_instance === null)
 		{
-			self::$_instance = new Gwilym_KeyStore_File();
+			self::$_instance = new Gwilym_Config_KeyStore::$defaultKeyStore;
 		}
 		return self::$_instance;
+	}
+
+	protected $_prefix;
+
+	protected $_prefixLocked = false;
+
+	public function prefix ($prefix = null)
+	{
+		if ($prefix === null) {
+			return $this->_prefix;
+		}
+
+		if ($this->_prefixLocked) {
+			return false;
+		}
+
+		$this->_prefix = (string)$prefix;
+		return true;
+	}
+
+	public function lockPrefix ()
+	{
+		$this->_prefixLocked = true;
 	}
 }
