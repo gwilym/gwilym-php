72cec4dc44cbf625b96d7c844ad31f788b48d475
gwilym <github@whoah.net>
2010-06-06 21:15:58 +1000

    some initial response class methods, not a final setup

--- lib/Gwilym/Response.php
+++ lib/Gwilym/Response.php
@@ -2,13 +2,35 @@
 
 class Gwilym_Response
 {
+	const STATUS_OK = 200;
+	const STATUS_MOVED_PERMANENTLY = 301;
+	const STATUS_SEE_OTHER = 303;
+	const STATUS_METHOD_NOT_ALLOWED = 405;
+
 	/**
 	* Send response to browser to redirect to another url.
 	*
 	* @param string $to URL to instruct browser to redirect to
+	* @param string $status http status code
 	*/
-	public function redirect ($to)
+	public function redirect ($to, $status = self::STATUS_SEE_OTHER)
 	{
+		header('Location: ' . $to, true, $status);
+		$this->end();
+	}
 
+	public function status ($status)
+	{
+		header('HTTP/1.1 ' . $status, true, $status);
+	}
+
+	public function header ($header, $value, $replace = true)
+	{
+		header($header . ': ' . $value, $replace);
+	}
+
+	public function end ()
+	{
+		exit;
 	}
 }
