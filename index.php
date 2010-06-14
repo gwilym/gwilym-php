313a2017992aa3e5c8ae4a8245a8f111441b820e
gwilym <github@whoah.net>
2010-06-14 21:22:50 +1000

    revert portion of 1e02d33b23b5e2e31ff7

--- index.php
+++ index.php
@@ -16,9 +16,3 @@ $request->addRouter('Gwilym_Router_Standard_Reverse');
 
 // go
 $request->handle();
-
-echo '<pre>';
-var_dump(1000 * (microtime(true) - GWILYM_START_TIME));
-var_dump(memory_get_usage());
-var_dump(memory_get_peak_usage());
-print_r(get_included_files());
