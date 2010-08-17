65e5a6bb298bc19e8039a829c86fd4c20332e503
gwilym <github@whoah.net>
2010-08-17 21:10:58 +1000

    adding FSM Persistable exceptions which I left out of previous commits

---
+++ lib/Gwilym/FSM/Persistable/Exception.php
@@ -0,0 +1,3 @@
+<?php
+
+class Gwilym_FSM_Persistable_Exception extends Gwilym_FSM_Exception { }
---
+++ lib/Gwilym/FSM/Persistable/Exception/NotFound.php
@@ -0,0 +1,3 @@
+<?php
+
+class Gwilym_FSM_Persistable_Exception_NotFound extends Gwilym_FSM_Persistable_Exception { }
