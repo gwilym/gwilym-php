577d99680d5345572f476a711be4cc7d6e7b38f5
gwilym <github@whoah.net>
2010-10-02 17:11:49 +1000

    controller must now be provided when instanciating a view fetch data from controller instead of storing in view

--- lib/Gwilym/View.php
+++ lib/Gwilym/View.php
@@ -2,9 +2,28 @@
 
 abstract class Gwilym_View
 {
-	public $data = array();
-
+	/**
+	 * @var Gwilym_Controller
+	 */
+	protected $_controller;
+	
+	/**
+	 * @return string
+	 */
 	abstract public function display ();
 
+	/**
+	 * @return void
+	 */
 	abstract public function render ();
+	
+	public function __construct (Gwilym_Controller $controller)
+	{
+		$this->_controller = $controller;
+	}
+	
+	public function data ()
+	{
+		return $this->_controller->data();
+	}
 }
