6f97d996afc4dbf44bf53b4ed46119f2cdeb2745
gwilym <github@whoah.net>
2010-10-02 17:26:06 +1000

    cleaned up Transfer exception implementation

--- lib/Gwilym/Request/Exception/Transfer.php
+++ lib/Gwilym/Request/Exception/Transfer.php
@@ -2,6 +2,26 @@
 
 class Gwilym_Request_Exception_Transfer extends Gwilym_Request_Exception
 {
-	public $to;
-	public $args;
+	protected $_to;
+	protected $_args = array();
+	
+	public function to ($value = null)
+	{
+		if (!func_num_args()) {
+			return $this->_to;
+		}
+		
+		$this->_to = (string)$value;
+		return $this;
+	}
+	
+	public function args ($value = null)
+	{
+		if (!func_num_args()) {
+			return $this->_args;
+		}
+		
+		$this->_args = $value;
+		return $this;
+	}
 }
