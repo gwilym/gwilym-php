ea8c71480bf9d76710f916baaf297a87238e0698
gwilym <github@whoah.net>
2010-10-04 10:20:17 +1100

    add comments update method definitions

--- lib/Gwilym/Router/Fixed.php
+++ lib/Gwilym/Router/Fixed.php
@@ -4,31 +4,44 @@ class Gwilym_Router_Fixed extends Gwilym_Router
 {
 	protected $_routes = array();
 
+	/**
+	 * Add a new URI => Controller route to this Fixed router.
+	 *
+	 * @param string $uri
+	 * @param Gwilym_Controller $controller
+	 * @return Gwilym_Router_Fixed
+	 */
 	public function addFixedRoute ($uri, $controller)
 	{
 		$this->_routes[$uri] = $controller;
+		return $this;
 	}
 
 	/**
-	* @param string $controller
-	* @param string $action
-	* @param array $args
-	* @return string
-	*/
-	public function routeToUri (Gwilym_Route $route)
+	 * Given a Route consisting of a Controller and various arguments, this method will return a URI which will
+	 * resolve to that controller. This is essentially the opposite of getRouteForRequest. This method always
+	 * returns false for this Fixed router since URIs are arbitrarily specified instead of generated.
+	 *
+	 * @param Gwilym_Route $route
+	 * @return string or false
+	 */
+	public function getUriForRoute (Gwilym_Route $route)
 	{
 		return false;
 	}
 
 	/**
-	* @param string $uri
-	* @return Gwilym_Route
-	*/
-	public function requestToRoute (Gwilym_Request $request)
+	 * Given a Request, this method will return a Route pointing to a valid Controller or false if none could be
+	 * resolved.
+	 *
+	 * @param Gwilym_Request $request
+	 * @return Gwilym_Route or false
+	 */
+	public function getRouteForRequest (Gwilym_Request $request)
 	{
-		$requestUri = $request->getUri();
+		$uri = $request->getUri();
 		foreach ($this->_routes as $routeUri => $controller) {
-			if ($requestUri === $routeUri) {
+			if ($uri === $routeUri) {
 				return new Gwilym_Route($request, $controller);
 			}
 		}
