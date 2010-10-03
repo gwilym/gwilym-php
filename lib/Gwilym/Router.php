3301f5e1c6c56bd164da6b3bbf63e5a1fe749b59
gwilym <github@whoah.net>
2010-10-04 10:19:58 +1100

    update method definitions

--- lib/Gwilym/Router.php
+++ lib/Gwilym/Router.php
@@ -11,22 +11,22 @@ abstract class Gwilym_Router
 	* @param array $args
 	* @return string
 	*/
-	abstract public function routeToUri (Gwilym_Route $route);
+	abstract public function getUriForRoute (Gwilym_Route $route);
 
 	/**
 	* @param string $uri
 	* @return Gwilym_Route
 	*/
-	abstract public function requestToRoute (Gwilym_Request $request);
+	abstract public function getRouteForRequest (Gwilym_Request $request);
 
 	public function defaultController ()
 	{
 		return 'Index';
 	}
 
-	public function route (Gwilym_Request $request)
+	public function routeRequest (Gwilym_Request $request)
 	{
-		$route = $this->requestToRoute($request);
+		$route = $this->getRouteForRequest($request);
 		if (!$route) {
 			return false;
 		}
