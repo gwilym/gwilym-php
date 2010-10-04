ed0d623524bc9afc21f81f3e27d957596415492a
gwilym <github@whoah.net>
2010-10-04 20:02:32 +1100

    paused fsm test

--- tests/Tests/Gwilym/FSM.php
+++ tests/Tests/Gwilym/FSM.php
@@ -2,46 +2,54 @@
 
 class Tests_Gwilym_FSM extends UnitTestCase
 {
-    public function testBasicFsm ()
-    {
-        $fsm = new Tests_Gwilym_FSM_BasicFsmTest;
-        $fsm->setPayload('0')
-            ->start();
-        $this->assertFalse($fsm->getStarted());
-        $this->assertEqual('0123', $fsm->getPayload());
-    }
-    
-    public function testStoppingFsm ()
-    {
-        $fsm = new Tests_Gwilym_FSM_StoppingFsmTest;
-        $fsm->setPayload('0')
-            ->start();
-        $this->assertFalse($fsm->getStarted());
-        $this->assertEqual('01', $fsm->getPayload());
-    }
-    
-    public function testLoopingFsm ()
-    {
-        $fsm = new Tests_Gwilym_FSM_LoopingFsmTest;
-        $fsm->setFrom(10)
-            ->setTo(20)
-            ->start();
-        $this->assertFalse($fsm->getStarted());
-        $this->assertEqual(20, $fsm->getCounter());
-        $this->assertEqual(10, $fsm->getIterations());
-    }
-    
-    public function testPausableFsm ()
-    {
-        $fsm = new Tests_Gwilym_Fsm_PausableTest;
-        $fsm->resume();
-        $this->assertTrue($fsm->getPaused());
-        $this->assertTrue($fsm->getStarted());
-        $fsm->resume();
-        $this->assertTrue($fsm->getPaused());
-        $this->assertTrue($fsm->getStarted());
-        $fsm->resume();
-        $this->assertFalse($fsm->getPaused());
-        $this->assertFalse($fsm->getStarted());
-    }
+	public function testBasicFsm ()
+	{
+		$fsm = new Tests_Gwilym_FSM_BasicFsmTest;
+		$fsm->setPayload('0')
+			->start();
+		$this->assertFalse($fsm->getStarted());
+		$this->assertEqual('0123', $fsm->getPayload());
+	}
+	
+	public function testStoppingFsm ()
+	{
+		$fsm = new Tests_Gwilym_FSM_StoppingFsmTest;
+		$fsm->setPayload('0')
+			->start();
+		$this->assertFalse($fsm->getStarted());
+		$this->assertEqual('01', $fsm->getPayload());
+	}
+	
+	public function testLoopingFsm ()
+	{
+		$fsm = new Tests_Gwilym_FSM_LoopingFsmTest;
+		$fsm->setFrom(10)
+			->setTo(20)
+			->start();
+		$this->assertFalse($fsm->getStarted());
+		$this->assertEqual(20, $fsm->getCounter());
+		$this->assertEqual(10, $fsm->getIterations());
+	}
+	
+	public function testPausableFsm ()
+	{
+		$fsm = new Tests_Gwilym_FSM_PausableTest;
+		$fsm->resume();
+		$this->assertTrue($fsm->getPaused());
+		$this->assertTrue($fsm->getStarted());
+		$fsm->resume();
+		$this->assertTrue($fsm->getPaused());
+		$this->assertTrue($fsm->getStarted());
+		$fsm->resume();
+		$this->assertFalse($fsm->getPaused());
+		$this->assertFalse($fsm->getStarted());
+	}
+	
+	public function testPausedFsm ()
+	{
+		$fsm = new Tests_Gwilym_FSM_PausedTest;
+		$fsm->start();
+		$this->assertTrue($fsm->getPaused());
+		$this->assertFalse($fsm->ran);
+	}
 }
---
+++ tests/Tests/Gwilym/FSM/PausedTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class Tests_Gwilym_FSM_PausedTest extends Gwilym_FSM_Paused
+{
+    public $ran = false;
+    
+    protected function _getStates ()
+    {
+        return array(
+            'go' => null,
+        );
+    }
+    
+    protected function _go ()
+    {
+        $this->ran = true;
+    }
+}
