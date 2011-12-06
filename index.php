545c64c14cb750005dfaf828944bd0e4a61d2c0d
gwilym <github@whoah.net>
2011-12-07 00:10:24 +1100

    committing something which has been staged since about dec 2010, a change in filenames and constants

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
 <?php
 
-require_once(dirname(__FILE__) . '/bootstrap.php');
+// include the Gwilym framework and tell it where the public request origin is
+define('GWILYM_PUBLIC_DIR', dirname(__FILE__));
+require_once(dirname(__FILE__) . '/lib/bootstrap.php');
 
+// create a new request handler
 $request = new Gwilym_Request;
 
-// fixed routes directly from / to Index
+/*
+// set up fixed routes directly from / to Index -- quickest way of loading a home page directly from known urls
 $fixed = new Gwilym_Router_Fixed;
 $fixed->addFixedRoute('', 'Controller_Index');
 $fixed->addFixedRoute('/', 'Controller_Index');
 $fixed->addFixedRoute('/index.php', 'Controller_Index');
 $request->addRouter($fixed);
 
-// fallback for dynamic routes using lazy router load
-$request->addRouter('Gwilym_Router_Standard_Reverse');
+// set up fallback for dynamic routes using lazy router load
+$request->addRouter('Gwilym_Router_Default');
+*/
 
-// go
+$router = new Gwilym_Router_Restful;
+$request->addRouter($router);
+
+// begin handling the request
 $request->handle();
