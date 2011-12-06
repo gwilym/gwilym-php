c301b2a55ac2260e9307d6536258a3769ae39d0f
gwilym <github@whoah.net>
2011-12-07 00:17:29 +1100

    minor interface updat and comments

--- lib/Gwilym/Router.php
+++ lib/Gwilym/Router.php
@@ -1,29 +1,46 @@
 <?php
 
+/**
+ * A Router's getRouteForRequest method is used by the Request class to produce a Route from the current Request to a
+ * Controller.
+ *
+ * The getUriForRoute method produces the opposite result -- given a Route, a Router will produce a URI which will map
+ * to that Route.
+ */
 abstract class Gwilym_Router
 {
 	/** @var Gwilym_Route The route which is currently being followed from request to controller */
 	protected $_currentRoute;
 	
 	/**
-	* @param string $controller
-	* @param string $action
-	* @param array $args
-	* @return string
+	* Map the given Route to a Request using the rules of this Router.
+	*
+	* @param Gwilym_Route $route
+	* @return Gwilym_Request
 	*/
-	abstract public function getUriForRoute (Gwilym_Route $route);
+	abstract public function getRequestForRoute (Gwilym_Route $route);
 
 	/**
-	* @param string $uri
+	* Map the given Request to a Route using the rules of this Router.
+	* 
+	* @param Gwilym_Request $request
 	* @return Gwilym_Route
 	*/
 	abstract public function getRouteForRequest (Gwilym_Request $request);
 
+	/**
+	 * The controller to use when no other can be successfully resolved.
+	 */
 	public function defaultController ()
 	{
 		return 'Index';
 	}
 
+	/**
+	 * Attempts to find and follow a Route for the given Request.
+	 *
+	 * @return bool
+	 */
 	public function routeRequest (Gwilym_Request $request)
 	{
 		$route = $this->getRouteForRequest($request);
@@ -31,9 +48,16 @@ abstract class Gwilym_Router
 			return false;
 		}
 		$this->_currentRoute = $route;
-		return $route->follow();
+		$result = $route->follow();
+		$this->_currentRoute = null;
+		return $result;
 	}
 	
+	/**
+	 * Returns the route currently being followed by this request, if any.
+	 *
+	 * @return Gwilym_Route
+	 */
 	public function getCurrentRoute ()
 	{
 		return $this->_currentRoute;
